5c1febeabd0cea8947abbcb415dd47dc7269e970
Keshav Rathinavel <keshavr@sahaj.ai>
2024-06-10 15:58:49 +0530

    Extract die rolling logic out to seperate interface

---
+++ src/GameDie.java
@@ -0,0 +1,7 @@
+package src;
+
+import java.util.Random;
+
+public interface GameDie {
+    Integer roll();
+}
---
+++ src/RandomDie.java
@@ -0,0 +1,11 @@
+package src;
+
+import java.util.Random;
+
+public class RandomDie implements GameDie {
+
+    @Override
+    public Integer roll() {
+        return new Random().nextInt(6) + 1;
+    }
+}
