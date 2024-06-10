09708c90fc41484d4971f99bf2c1e89d67837721
Keshav Rathinavel <keshavr@sahaj.ai>
2024-06-10 16:20:11 +0530

    Create programmable die in test

--- test/SnakesAndLaddersTest.java
+++ test/SnakesAndLaddersTest.java
@@ -1,9 +1,21 @@
 package test;
 
 import org.junit.jupiter.api.Test;
-import src.SnakesAndLadders;
+import src.GameDie;
 
 class SnakesAndLaddersTest {
+
+    ProgrammableDie programmableDie = new ProgrammableDie();
+
+    static class ProgrammableDie implements GameDie {
+        Integer index = 0;
+        Integer[] moves = {5,3,2,4,2,4,5,6,4,6,3,3,1,6,5,3,3,4,3,3,1,4,5,5,3,6,2,1,5,4,2,1,4,5,1,4,2,1,6,4,3,1,1,4,5,1,4,5,4,6,6,6,4,6,1,5,2,1,6,3,1,4,6,1};
+
+        @Override
+        public Integer roll() {
+            return moves[index];
+        }
+    }
 
     @Test
     void testRun() {
